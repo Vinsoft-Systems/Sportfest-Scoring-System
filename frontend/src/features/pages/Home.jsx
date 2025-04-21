6cda6fec8c0cbc64d8fa83e86ab4b2b3b32a8901
Vinston Salim <vinston.salim@outlook.com>
2025-04-21 17:18:33 +0200

    example

--- frontend/src/features/pages/Home.jsx
+++ frontend/src/features/pages/Home.jsx
@@ -1,9 +1,27 @@
 import { Center, Title } from '@mantine/core';
+import { ApiProvider, useApi } from 'fastapi-rtk';
+
+function Data(){
+  const { data } = useApi();
+  return (
+    <div>
+      <h1>Data</h1>
+      <p>This is the data page.</p>
+      <p>Data: {JSON.stringify(data)}</p>
+    </div>
+  );
+}
 
 export default function Home() {
+
+
   return (
+
     <Center>
       <Title order={1}>Home</Title>
+      <ApiProvider resource_name='competitions'>
+        <Data />
+      </ApiProvider>
     </Center>
   );
 }
