5911bc3c09325171e2e7f7835280d50b3a180950
Vinston Salim <vinston.salim@outlook.com>
2025-05-22 23:46:26 +0200

    Change DB into postgres

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/scoring-system-db'
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///./app.db"
+# SQLALCHEMY_DATABASE_URI = "sqlite:///./app.db"
